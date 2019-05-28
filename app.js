6650388e941799d2ae8abee1da3ca3d3fb8e5f54
AngelLS24 <angls2495@gmail.com>
2019-05-27 21:07:01 -0500

    indice de la pagina

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var alumnosRouter = require('./routes/alumnos');
+var apartadosRouter = require('./routes/apartados')
 
 var app = express();
 
@@ -21,6 +23,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/alumnos', alumnosRouter);
+app.use('/apartados', apartadosRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
