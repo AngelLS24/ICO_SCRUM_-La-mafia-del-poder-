529df3f980b466760100a8f5837ea09581affd4e
MisaelTR64 <44656557+MisaelTR64@users.noreply.github.com>
2019-05-27 22:20:35 -0500

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var alumnosRouter = require('./routes/alumnos');
 var defbasRouter = require('./routes/definiciones_basicas');
+var defutRouter = require ('./routes/defut');
 var apartadosRouter = require('./routes/apartados')
 
 var app = express();
@@ -27,6 +28,7 @@ app.use('/users', usersRouter);
 app.use('/alumnos', alumnosRouter);
 app.use('/apartados', apartadosRouter);
 app.use('/def_bas', defbasRouter);
+app.use('/defut', defutRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
