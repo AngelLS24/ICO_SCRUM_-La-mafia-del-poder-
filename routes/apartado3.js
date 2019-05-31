8526492d2221fc095bf5bec64fa48efa53fb7534
crisbdis <cristopher.venom@gmail.com>
2019-05-30 23:18:20 -0500

    Pagina de los roles de Scrum y modificacion en pagina de CRM

--- routes/apartado3.js
+++ routes/apartado3.js
@@ -13,4 +13,7 @@ router.get('/MIS', function(req, res, next){
 	res.render('paginas/apartado3/CRM', { title: 'CRM' });
 });
 
+router.get('/roles', function(req, res, next){
+	res.render('paginas/apartado3/roles_scrum', { title: 'Roles' });
+});
 module.exports = router;
