d6b1f1cd3902beea83fd181ca12cfc07f501f802
AngelLS24 <angls2495@gmail.com>
2019-05-30 22:44:09 -0500

    actualizacion de archivos, y que es scrum

--- routes/apartado1.js
+++ routes/apartado1.js
@@ -1,16 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/TPS', function(req, res, next) {
-  	res.render('paginas/apartado1/sistemas_de_información_transaccionales', { title: 'TPS' });
+router.get('/componentes_basicos_de_un_si', function(req, res, next) {
+  	res.render('paginas/apartado1/componentes_basicos_de_un_sistema_de_informacion', { title: 'Componentes Básicos de un SI' });
 });
 
-router.get('/CRM', function(req, res, next){
-	res.render('paginas/apartado1/CRM', { title: 'CRM' });
+router.get('/definiciones_basicas', function(req, res, next){
+	res.render('paginas/apartado1/definiciones_basicas', { title: 'Definiciones Básicas' });
 });
 
-router.get('/MIS', function(req, res, next){
-	res.render('paginas/apartado1/CRM', { title: 'CRM' });
+router.get('/informacion_util', function(req, res, next){
+	res.render('paginas/apartado1/infut', { title: 'información Útil' });
 });
 
 module.exports = router;
